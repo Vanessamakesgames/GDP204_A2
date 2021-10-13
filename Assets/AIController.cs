aefac69d43cf609dc6e1c609aa50730227d220a0
Van Hondow <vanessa.hondow@outlook.com>
2021-10-13 21:06:05 +1100

    the required summary

---
+++ Assets/AIController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    // CHANGE CODE TO USE A STATE MACHINE + RUN BEHAVIOURS THROUGH STATE MACHINE
+
+
+    public GameObject[] checkpoints;
+    Transform currentTarget;
+    public float threshold;
+    public float detectionRange;
+    public GameObject player; //Change to initialise in Start() + make private
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Patrol();
+    }
+
+    // Change into a state machine model
+    void Patrol()
+    {
+        //keep moving to target + check for player + raycast to see if AI can see player
+        float distToPlayer = Vector3.Distance(player.transform.position, this.transform.position);
+        if (distToPlayer <= detectionRange) //if player in range of AI
+        {
+            //raycast to see if AI can see player or if obstructed by scenery
+            if (/*raycast to see if AI can see player*/)
+            {
+
+            }
+            else // AI cannot see player. Keep patrolling
+            {
+                float distToObj = Vector3.Distance(currentTarget.position, this.transform.position);
+                if (distToObj >= threshold)
+                {
+
+                }
+                else
+                {
+                    //Change target checkpoint
+                }
+            }
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float moveDirection;
+    BoxCollider2D bc2d;
+    bool onGround, facingRight;
+    Rigidbody2D rb2d;
+    Transform t;
+    Vector3 cameraPos;
+
+    [Header ("Camera")]
+    [SerializeField]
+    Camera mainCamera;
+
+    [Header ("Player movement stats")]
+    [SerializeField]
+    float health;
+    [SerializeField]
+    float walkSpeed;
+    [SerializeField]
+    float runSpeed;
+    [SerializeField]
+    float jumpHeight;
+    [SerializeField]
+    float groundDeceleration;
+    [SerializeField]
+    float fallMultiplier;
+    [SerializeField]
+    float lowJumpMultiplier;
+
+    [Header("Ground Checker")]
+    [SerializeField]
+    public LayerMask groundLayer;
+    [SerializeField]
+    public Transform groundCheckerObj;
+    [SerializeField]
+    float checkerRadius;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = transform;
+        health = 100.0f;
+        bc2d = GetComponent<BoxCollider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.freezeRotation = true;
+        moveDirection = 0f;
+        walkSpeed = 2.5f;
+        runSpeed = 5f;
+        facingRight = true;
+        jumpHeight = 5f;
+        onGround = false;
+        fallMultiplier = 2.5f;
+        lowJumpMultiplier = 2f;
+        checkerRadius = 0.01f;
+
+        if (mainCamera)
+        {
+            cameraPos = mainCamera.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckForGround();
+        Jump();
+        BetterJump();
+        Collisions();
+        Camera();      
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy;
+        if (Input.GetKey(KeyCode.LeftShift)) moveBy = x * runSpeed;
+        else moveBy = x * walkSpeed;
+        rb2d.velocity = new Vector2(moveBy, rb2d.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (onGround)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpHeight);
+                onGround = false;
+                Debug.Log("Jump");
+            }
+        }
+    }
+
+    private void BetterJump()
+    {
+        if (rb2d.velocity.y < 0) rb2d.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        else if (rb2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space)) rb2d.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+    }
+
+    private void Camera()
+    {
+        if (mainCamera)
+        {
+            mainCamera.transform.position = new Vector3(t.position.x, t.position.y, cameraPos.z);
+        }
+    }
+
+    private void CheckForGround()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(groundCheckerObj.position, checkerRadius, groundLayer);
+        if (hit != null) onGround = true;
+        else onGround = false;
+    }
+
+    private void Collisions()
+    {
+
+    }
+}
